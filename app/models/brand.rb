7a5abd717d2b67c73ca5f681087ed0a3582868d2
anmacagno <anmacagno@gmail.com>
2023-03-08 00:19:10 -0600

    Implements average_price for brands

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -4,4 +4,11 @@ class Brand < ApplicationRecord
   has_many :models, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def average_price
+    prices = models.filter_map(&:average_price)
+    return 0 if prices.empty?
+
+    prices.sum / prices.size
+  end
 end
--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -13,4 +13,28 @@ RSpec.describe Brand do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   end
+
+  describe '#average_price' do
+    subject(:brand) { create(:brand) }
+
+    context 'when the brand has models' do
+      before do
+        create(:model, brand:, name: 'F-150', average_price: 268_727)
+        create(:model, brand:, name: 'F-250', average_price: 201_785)
+        create(:model, brand:, name: 'F-350', average_price: 287_341)
+        create(:model, brand:, name: 'F-450', average_price: nil)
+        create(:model, brand:, name: 'F-550', average_price: nil)
+      end
+
+      it 'returns the correct average price' do
+        expect(brand.average_price).to eq(252_617)
+      end
+    end
+
+    context 'when the brand does not have models' do
+      it 'returns 0' do
+        expect(brand.average_price).to eq(0)
+      end
+    end
+  end
 end
