5089e5fd1c3900a253b1ce6e43c46a557b5ba9dc
anmacagno <anmacagno@gmail.com>
2023-03-09 11:54:12 -0600

    Implements seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'net/http'
+
+MODELS_URL = 'https://raw.githubusercontent.com/remigioamc/nexu-backend-test/master/models.json'
+
+Rails.logger = Logger.new($stdout)
+Rails.logger.level = Logger::INFO
+
+response = Net::HTTP.get(URI(MODELS_URL))
+models = JSON.parse(response, symbolize_names: true)
+
+Rails.logger.info("Processing #{models.size} models...")
+
+models.each do |model|
+  brand = Brand.find_or_create_by!(name: model[:brand_name])
+  brand.models.create!(name: model[:name], average_price: model[:average_price])
+rescue StandardError => e
+  Rails.logger.error("Error inserting model with id #{model[:id]}. #{e.message}.")
+end
